63e74207f98f0d9641795958a7e9a151b1943618
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-11-26 20:35:25 +0800

    representing code of crafting interpreters

--- src/com/jam/tool/GenerateAst.java
+++ src/com/jam/tool/GenerateAst.java
@@ -33,6 +33,8 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // The AST classes.
         for (String type : types) {
             String className = type.split(":")[0].trim();
@@ -62,6 +64,13 @@ public class GenerateAst {
 
         writer.println("    }");
 
+        // Visitor pattern.
+        writer.println();
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
         // Fields.
         writer.println();
         for (String field: fields) {
@@ -69,6 +78,19 @@ public class GenerateAst {
         }
 
         writer.println("  }");
+    }
+
+    private static void defineVisitor (
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("   }");
     }
 
 }
